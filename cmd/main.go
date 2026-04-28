bd487211ce2292a668ec465ccfb2e307fdbc972e
SV5623 <ksv05623@gmail.com>
2026-04-28 17:08:44 +0200

    Database+ migration working via 'goose'

--- cmd/main.go
+++ cmd/main.go
@@ -1,26 +1,40 @@
 package main
 
 import (
+	"api_project/internal/env"
+	"context"
 	"log"
 	"log/slog"
 	"os"
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	_ = godotenv.Load()
+	ctx := context.Background()
+
 	cfg := &Config{
 		addr : ":2000",
-		db: dbConfig{},
+		db: dbConfig{		
+			dsn: env.GetString("GOOSE_DBSTRING", ""),
+		},	
+	}	
+	if cfg.db.dsn == "" {
+		log.Fatal("GOOSE_DBSTRING is not set")
 	}
+	//database
+	conn, err := pgx.Connect(ctx, cfg.db.dsn)
+	if err != nil {
+		log.Printf("Помилка підключення до бази даних: %s", err)
+		os.Exit(1)
+	}
+	defer conn.Close(ctx)
 
 	api := aplication{
 	config: *cfg,
 	}
-
-	// Logger
-	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	
-	slog.SetDefault(logger)
-
 	h := api.mount() //повертає хендлер який ми будемо запускати
 	if err := api.run(h); err != nil { // запускає сервер і слухає на вказаному адресі
 		// slog.Error("Сервер не вдалося запустити:, помилка %s", err) 
@@ -28,6 +42,9 @@ func main() {
 		// log.Println("Сервер не вдалося запустити:, помилка %s", err)
 		os.Exit(1)
 	}
+	// Logger
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
 
 	// або мож написати інакше
 	// if err := api.run(api.mount()); err != nil { // запускає сервер і слухає на вказаному адресі
--- go.mod
+++ go.mod
@@ -2,4 +2,20 @@ module api_project
 
 go 1.26.2
 
-require github.com/go-chi/chi/v5 v5.2.5
+require (
+	github.com/go-chi/chi/v5 v5.2.5
+	github.com/jackc/pgx v3.6.2+incompatible
+)
+
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
+
+require (
+	github.com/jackc/pgx/v5 v5.9.2
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.50.0 // indirect
+	golang.org/x/text v0.36.0 // indirect
+)
